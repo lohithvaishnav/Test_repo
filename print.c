909dbf5286e085aa00f83712dfa9914c9c59a9d9
lohithvaishnav <30681893+lohithvaishnav@users.noreply.github.com>
2018-02-04 09:11:15 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void blockg(void);   /*Prototype for blockg function */
+
+int main (void) {
+   printf("\n");
+   blockg();
+   printf("\n");
+}
+
+/* Print out the Block letter g */
+void blockg(void) {
+  printf("gggggg\n");
+  printf("g    g\n");
+  printf("g\n");
+  printf("g  ggg\n");
+  printf("g    g\n");
+  printf("gggggg\n");
+}
+ 
